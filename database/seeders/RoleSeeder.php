0d19eaf5da6bd0ee347bda7fd2d090c7c19959be
skabraha <skabraha77@gmail.com>
2022-09-12 17:38:42 -0500

    se siembran usuarios a la base de datos con su rol

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
@@ -21,7 +23,12 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name' => 'admin.home'])->syncRoles($role1);
         Permission::create(['name' => 'admin.user.home'])->syncRoles([$role1, $role2]);
-        // Permission::create(['name' => 'auth.login'])->syncRoles([$role1, $role2]);
 
+        User::create([
+            'firstname' => 'Abraham Eduardo',
+            'lastname' => 'Diaz Torres',
+            'username' => '7141757',
+            'password' => Hash::make(7141757)
+        ])->assignRole('Admin');
     }
 }
