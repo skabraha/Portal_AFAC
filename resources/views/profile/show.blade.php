f13a04c142cd4f7e75966f67a4e3a9637e0d220b
jessicasoto01afac <jessica.soto928@gmail.com>
2022-09-13 17:25:11 -0500

    mas cambios

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,27 +1,33 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    <div class="py-8 px-4 lg:px-14">
 
+        <h1 class="text-lg font-semibold text-left text-gray-800 capitalize xs:text-xl lg:text-3xl dark:text-white">
+            Actualización de Contraseña
+        </h1>
+
+        <div class="flex justify-left mx-auto mt-6">
+            <span class="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+            <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+            <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+        </div>
+    </div>
     <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-8 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+            @livewire('profile.update-profile-information-form')
 
-                <x-jet-section-border />
+            <x-jet-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.update-password-form')
+            </div>
 
-                <x-jet-section-border />
+            <x-jet-section-border />
             @endif
 
-            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+            {{--  @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.two-factor-authentication-form')
                 </div>
@@ -40,6 +46,6 @@
                     @livewire('profile.delete-user-form')
                 </div>
             @endif
+        </div> --}}
         </div>
-    </div>
 </x-app-layout>
