e3817f705d2ca4bc1d4ec4a0e1d1c769cbdf832f
jessicasoto01afac <jessica.soto928@gmail.com>
2022-09-13 11:04:28 -0500

    qssqsqsqs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,11 +93,12 @@
                                         class="text-center inline-block px-3 py-2 justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                         {{ __('Iniciar Sesión') }}
                                     </x-jet-button>
-
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 </x-guest-layout>
