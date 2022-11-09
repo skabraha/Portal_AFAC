871fcc46b7d2e5c5a1f1509e87de472f457c237d
jessicasoto01afac <jessica.soto928@gmail.com>
2022-11-09 17:12:21 -0600

    mas

--- resources/views/admin/access.blade.php
+++ resources/views/admin/access.blade.php
@@ -9,7 +9,21 @@
         </div>
     </div>
     @endif
-    <div class="py-12">
+    <nav class="border-b-2 border-gray-200 m:px-6 lg:px-8 flex space-x-8">
+        <a class="py-4 px-1 inline-flex items-center gap-2 text-sm whitespace-nowrap text-gray-400 hover:text-blue-600"
+            href="{{ route('busqueda') }}">
+            Comprobantes de pago
+        </a>
+        <a class="py-4 px-1 inline-flex items-center gap-2 text-sm font-medium whitespace-nowrap text-gray-400 hover:text-blue-600"
+            href="{{ route('admin_import') }}" aria-current="page">
+            Importación
+        </a>
+        <a class="py-4 px-1 inline-flex items-center gap-2 text-sm whitespace-nowrap text-gray-400 hover:text-blue-600"
+            href="{{ route('bitacora') }}">
+            Historial de descarga
+        </a>
+    </nav>
+    <div class="py-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div x-data="{ open: false }">
