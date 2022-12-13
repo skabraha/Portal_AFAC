7b5a48293fe6cf311d1c9efa20e2384961dcb136
jessicasoto01afac <jessica.soto928@gmail.com>
2022-12-13 10:13:16 -0600

    pruebas xml

---
+++ app/Models/prubas_xml.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class prubas_xml extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_12_13_161254_create_prubas_xmls_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prubas_xmls', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prubas_xmls');
+    }
+};
