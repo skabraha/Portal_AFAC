047c56b6c16e8d003db364f8d6a4e9f8b719996d
jessicasoto01afac <jessica.soto928@gmail.com>
2022-12-13 10:19:15 -0600

    CAMBUSO

--- app/Http/Controllers/ReadXmlController.php
+++ app/Http/Controllers/ReadXmlController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\XmlData;
+use App\Models\prubas_xml;
 use SimpleXMLElement;
 
 class ReadXmlController extends Controller
