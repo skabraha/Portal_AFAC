9b0ede80c895b60606b117fda8724205a3293bd6
jessicasoto01afac <jessica.soto928@gmail.com>
2022-12-21 15:07:11 -0600

    cambio de xml po csv

--- resources/views/admin/import.blade.php
+++ resources/views/admin/import.blade.php
@@ -25,8 +25,7 @@
                                     id="bar-with-underline-item-1" data-hs-tab="#bar-with-underline-1"
                                     :class="{ 'text-blue-600 border-b-blue-600 text-gray-900': openTab == 1 }"
                                     aria-controls="bar-with-underline-1" role="tab">
-                                    Administrador de xml
-                                </button>
+                                    Administrador de csv                                </button>
                                 <button type="button" @click="openTab = 2"
                                     class="hs-tab-active:border-b-blue-600 hs-tab-active:text-gray-900 dark:hs-tab-active:text-white dark:hs-tab-active:border-b-blue-600 relative min-w-0 flex-1 bg-white first:border-l-0 border-l border-b-2 py-4 px-4 text-gray-500 hover:text-gray-700 xs:text-xs text-base font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 dark:bg-gray-800 dark:border-l-gray-700 dark:border-b-gray-700 dark:hover:bg-gray-700 dark:hover:text-gray-400 dark:hover:text-gray-300"
                                     id="bar-with-underline-item-2" data-hs-tab="#bar-with-underline-2"
